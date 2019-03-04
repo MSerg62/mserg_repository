4f6c3d90f4372856937ed03bb11d242965301d40
Sergey <sergei.mamaev@gmail.com>
2019-03-04 18:30:39 +0300

    /

---
+++ CIS/CIS/Controllers/EHCardController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessRules;
+
+namespace CIS.Controllers
+{
+    public class EHCardController : Controller
+    {
+        // GET: EHCard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
