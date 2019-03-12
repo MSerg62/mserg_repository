21ff90e8902cf95b685bca6d10e164b21865e722
Sergey <sergei.mamaev@gmail.com>
2019-03-12 09:54:24 +0300

    /

--- CIS/BusinessRules/Entities/HCard.cs
+++ CIS/BusinessRules/Entities/HCard.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BusinessRules.Enum;
 
-namespace BusinessRules.Entity
+
+namespace BusinessRules
 {
     /// <summary>
     /// история болезни для случая госпитализации
--- CIS/BusinessRules/Enum/PersonIdDocument.cs
+++ CIS/BusinessRules/Enum/PersonIdDocument.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessRules.Enum
+namespace BusinessRules
 {
     public enum PersonIdDocument
     {
