9d3db68b0d332a972949b6c32f69d3375f5ec0e1
Sergey <sergei.mamaev@gmail.com>
2019-03-14 18:00:29 +0300

    .

---
+++ CIS/BusinessRules/BusinessModules/EntitiesImp/PatientImp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRules.BusinessModules.EntitiesImp
+{
+    class PatientImp : Patient
+    {
+        public string Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string PatientId { get; set; }
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime BirthDate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string FirstName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public PersonIdDocument PatientIdDocument { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DocumentAddress { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string RealAddress { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Gender GenderItem { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
